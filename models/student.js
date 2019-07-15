4d2637019a7ec9b69fff57e96a986e8eaf8959c4
hudabz <huda_bz@hotmail.com>
2019-07-15 21:44:27 +0300

    HW week 10 day 1, I have error in the virtual

--- models/student.js
+++ models/student.js
@@ -10,23 +10,41 @@
     // * age - (Number, Greater than or equal 18)
     // * city - (String)
 
+    const mongoose = require("mongoose"); 
 
+    const studentSchema = new mongoose.Schema({
+        
+            firstName: {
+                 type: String, 
+                 required: true
+            }, 
+            lastName: {
+                type: String, 
+                 required: true
+            },
+            grade: {
+                type: Number, 
+                required: true
+            }, 
+            age: {
+                type: Number, 
+                min: 18
+            }, 
+            city: {
+                type: String
+            }
+         
+    },{
+       timestamps:true 
+    })
 
-
-
-
-
-
-
-
-    
 
 
 // The model of the schema 
 
-
+const Student = mongoose.model("Student", studentSchema) 
 
 
 // Export the model 
-
+module.exports = Student
 
